1a31f08621c25519d05840e69632cf02db12cbc8
donizk <donizk@allegheny.edu>
2022-09-25 23:22:00 -0400

    writing database code

---
+++ files/database-code-2.sql
@@ -0,0 +1,52 @@
+/* CPS SQLITE DATABASE CODE - SINGLE*/
+
+/* EduAttain */
+
+/* 
+VARIABLES KEY:
+
+YEAR 
+SERIAL - household serial #
+MONTH
+STATEFIP - state fips code
+PERNUM - numbered count of individual in HH
+WTFINL - final basic weight -- for stats
+CPSIDP - unique individual identifier
+AGE
+SEX
+RACE
+BPL - birthplace
+HISPAN - hispanic
+EDUC - educational attainment
+EDDIPGED - HS or GED
+FTOTVAL - total family income
+INCTOT - total personal income
+/*
+
+/* Remove previous table if already in database */
+DROP TABLE CPS;
+CREATE TABLE CPS (
+CPSIDP VARCHAR NOT NULL PRIMARY KEY,
+YEAR VARCHAR NOT NULL,
+MONTH VARCHAR NOT NULL,
+STATEFIP VARCHAR NOT NULL,
+PERNUM VARCHAR NOT NULL,
+WTFINL VARCHAR NOT NULL,
+ASECWT VARCHAR NOT NULL,
+AGE VARCHAR NOT NULL,
+SEX VARCHAR NOT NULL,
+RACE VARCHAR NOT NULL,
+BPL VARCHAR NOT NULL,
+HISPAN VARCHAR NOT NULL,
+EDUC VARCHAR NOT NULL,
+FTOTVAL VARCHAR NOT NULL,
+INCTOT VARCHAR NOT NULL
+);
+
+/* file import commands - csv */
+
+/* assign import mode */
+.mode csv
+
+/* import csv file in */
+.import C:\Users\kyrie\OneDrive\Desktop\data\cps_00002.csv CPS
