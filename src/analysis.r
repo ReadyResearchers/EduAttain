b77edafac1c543171665b3fea7c49a9c9289664f
donizk <donizk@allegheny.edu>
2022-09-30 21:03:10 -0400

    adding updates -- still trying to debug

--- src/analysis.r
+++ src/analysis.r
@@ -2,14 +2,29 @@
 # ANALYSIS SOURCE CODE
 # Kyrie Doniz, CS600, Fall 2022
 
+########################################################
+
+rm(list = ls()) # remove variables stored in memory.
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
+
+
+########################################################
+
 # install necessary packages
+install.packages("devtools") # install to connect to github
+devtools::install_github("RcppCore/Rcpp") # download needed dependency for DBI
+devtools::install_github("rstats-db/DBI") # download new version of DBI to run SQLite
 install.packages("RSQLite")
 
 # load necessary libraries
 library(RSQLite)
 
 # connect to database
-conn <- dbConnect(RSQLite::SQLite(), "CPS.db")
+conn <- dbConnect(RSQLite::SQLite(), "CPS")
 
 # list all tables connected to db
 dbListTables(conn)
