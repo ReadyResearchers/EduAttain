ce60f2de2d136119078094c3ef725a2134dddd6c
donizk <donizk@allegheny.edu>
2022-12-19 18:01:34 -0500

    adding to readme

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
+![eduattain header](files/eduattain.jpg)
+
 # EduAttain: A Statistical Analysis of the Impact of Different Demographic Indicators on Educational Attainment
 
 ## Abstract
 
 In recent years, trends in educational attainment have reflected simultaneous movements towards closing and widening disparities between different identity groups. Studying educational attainment, specifically revolved around studying differences or disparities in educational attainment, is important because of the implications it has for future work opportunities, financial security, and resource access. This study will look to answer the question of what specific demographic factors and to what degree they are determinants of educational attainment. The demographic factors that this study will focus on include sex, race/ethnicity, and income. The main goal of this study is to address the lack of research in comparing educational attainment trends between different identity groups, with special interest in studying differences in race/ethnicity, and income. Leveraging data from IPUMS, and using R and SQLite, trends in educational attainment across different identity groups will be studied in order to answer this question and display results on a data dashboard, in order to draw comparisons.
+
+## Goal + Progress
+
+*Main Goal:* create a data dashboard displaying statistics and visualizations of trends of educational attainment across different socioeconomic indicators for comparison
+
+*Completed Work*:
+[X] Database
+[-] R visualizations
+[-] R statistics
+[*] R regression
+[*] R Shiny Web Platform
+
+(X = completed, - = in progress, * = spring sem)
+
+## Installation
+
+## Running EduAttain
--- src/analysis.r
+++ src/analysis.r
@@ -47,14 +47,17 @@ result = result[-1,]
 # display data
 head(result)
 
+View(result)
+
 # looking at yr ranges in data
 rng <- range(result$YEAR)
 rng
 
+
 ###### 
 
 # query to get all data from 2014 for sex + educ attain
-q1 <- 'SELECT cpsidp, sex, educ, month, age, statefip FROM CPS WHERE year = 2014'
+q1 <- 'SELECT cpsidp, sex, educ, race, hispan, ftotval, inctot, month, age, statefip FROM CPS WHERE year = 2014'
 data2014 <- dbGetQuery(conn, q1)
 # test to check if it is a dataframe --> need to remove
 data2014
@@ -66,11 +69,12 @@ head(data2014)
 # view data sets
 View(data2014)
 
+
 ###### 
 
 # query to get all data from 2010 for sex + educ attain
-q2 <- 'SELECT cpsidp, sex, educ, month, age, statefip FROM CPS WHERE year = 2010'
-data2010 <- dbGetQuery(conn, q1)
+q2 <- 'SELECT cpsidp, sex, educ, race, hispan, ftotval, inctot, month, age, statefip FROM CPS WHERE year = 2010'
+data2010 <- dbGetQuery(conn, q2)
 # test to check if it is a dataframe --> need to remove
 data2010
 class(data2010)
@@ -80,6 +84,22 @@ head(data2010)
 
 # view data sets
 View(data2010)
+
+
+########
+
+# query to get all data from 2010 for sex + educ attain
+q3 <- 'SELECT cpsidp, sex, educ, month, age, statefip FROM CPS WHERE year = 2012'
+data2012 <- dbGetQuery(conn, q1)
+# test to check if it is a dataframe --> need to remove
+data2012
+class(data2012)
+
+# display data
+head(data2012)
+
+# view data sets
+View(data2012)
 
 #################################
 # Filter Data / Styling Data for Visuals
@@ -125,44 +145,129 @@ data2014$EDUC[data2014$EDUC == "81"]<-"Some college, no degree"
 data2014$EDUC[data2014$EDUC == "91"]<-"Occupational/Vocational Program Degree"
 data2014$EDUC[data2014$EDUC == "92"]<-"Associate's Degree, Academic"
 
+# all yrs
+result$EDUC[result$EDUC == "10"]<-"Grades 1-4"
+result$EDUC[result$EDUC == "111"]<-"Bachelor's Degree"
+result$EDUC[result$EDUC == "123"]<-"Master's Degree"
+result$EDUC[result$EDUC == "124"]<-"Professional School Degree"
+result$EDUC[result$EDUC == "125"]<-"Doctorate Degree"
+result$EDUC[result$EDUC == "2"]<-"None/Preschool/Kindergarten"
+result$EDUC[result$EDUC == "20"]<-"Grades 5-6"
+result$EDUC[result$EDUC == "30"]<-"Grades 7-8"
+result$EDUC[result$EDUC == "40"]<-"HS, Grade 9"
+result$EDUC[result$EDUC == "50"]<-"HS, Grade 10"
+result$EDUC[result$EDUC == "60"]<-"HS, Grade 11"
+result$EDUC[result$EDUC == "71"]<-"HS, Grade 12, no diploma"
+result$EDUC[result$EDUC == "73"]<-"HS Diploma or Equiv."
+result$EDUC[result$EDUC == "81"]<-"Some college, no degree"
+result$EDUC[result$EDUC == "91"]<-"Occupational/Vocational Program Degree"
+result$EDUC[result$EDUC == "92"]<-"Associate's Degree, Academic"
+
 
 # replacing state fips code with state name
-data2010$STATEFIP[data2010$STATEFIP == "6"]<-"CA"
-data2010$STATEFIP[data2010$STATEFIP == "48"]<-"TX"
-data2010$STATEFIP[data2010$STATEFIP == "36"]<-"NY"
-data2010$STATEFIP[data2010$STATEFIP == "12"]<-"FL"
-data2010$STATEFIP[data2010$STATEFIP == "17"]<-"IL"
-data2010$STATEFIP[data2010$STATEFIP == "42"]<-"PA"
-data2010$STATEFIP[data2010$STATEFIP == "39"]<-"OH"
-data2010$STATEFIP[data2010$STATEFIP == "24"]<-"MD"
-data2010$STATEFIP[data2010$STATEFIP == "26"]<-"MI"
-data2010$STATEFIP[data2010$STATEFIP == "27"]<-"MN"
-data2010$STATEFIP[data2010$STATEFIP == "8"]<-"CO"
-data2010$STATEFIP[data2010$STATEFIP == "13"]<-"GA"
-data2010$STATEFIP[data2010$STATEFIP == "51"]<-"VA"
-data2010$STATEFIP[data2010$STATEFIP == "9"]<-"CT"
-data2010$STATEFIP[data2010$STATEFIP == "37"]<-"NC"
-data2010$STATEFIP[data2010$STATEFIP == "34"]<-"NJ"
+data2010$STATEFIP[data2010$STATEFIP == "6"]<-"CA" #CALIFORNIA
+data2010$STATEFIP[data2010$STATEFIP == "48"]<-"TX" #TEXAS
+data2010$STATEFIP[data2010$STATEFIP == "36"]<-"NY" #NEW YORK
+data2010$STATEFIP[data2010$STATEFIP == "12"]<-"FL" #FLORIDA
+data2010$STATEFIP[data2010$STATEFIP == "17"]<-"IL" #ILLINOIS
+data2010$STATEFIP[data2010$STATEFIP == "42"]<-"PA" #PENNSYLVANIA
+data2010$STATEFIP[data2010$STATEFIP == "39"]<-"OH" #OHIO
+data2010$STATEFIP[data2010$STATEFIP == "24"]<-"MD" #MARYLAND
+data2010$STATEFIP[data2010$STATEFIP == "26"]<-"MI" #MICHIGAN
+data2010$STATEFIP[data2010$STATEFIP == "27"]<-"MN" #MINNESOTA
+data2010$STATEFIP[data2010$STATEFIP == "8"]<-"CO" #COLORADO
+data2010$STATEFIP[data2010$STATEFIP == "13"]<-"GA" #GEORGIA
+data2010$STATEFIP[data2010$STATEFIP == "51"]<-"VA" #VIRGINIA
+data2010$STATEFIP[data2010$STATEFIP == "9"]<-"CT" #CONNECTICUT
+data2010$STATEFIP[data2010$STATEFIP == "37"]<-"NC" #NORTH CAROLINA
+data2010$STATEFIP[data2010$STATEFIP == "34"]<-"NJ" #NEW JERSEY
+data2010$STATEFIP[data2010$STATEFIP == "33"]<-"NH" #NEW HAMPSHIRE
+data2010$STATEFIP[data2010$STATEFIP == "53"]<-"WA" #WASHINGTON
+data2010$STATEFIP[data2010$STATEFIP == "19"]<-"IA" #IOWA
+data2010$STATEFIP[data2010$STATEFIP == "15"]<-"HI" #HAWAII
+data2010$STATEFIP[data2010$STATEFIP == "55"]<-"WI" #WISCONSIN
+data2010$STATEFIP[data2010$STATEFIP == "29"]<-"MO" #MISSOURI
+data2010$STATEFIP[data2010$STATEFIP == "10"]<-"DE" #DELAWARE
+data2010$STATEFIP[data2010$STATEFIP == "32"]<-"NV" #NEVADA
+data2010$STATEFIP[data2010$STATEFIP == "18"]<-"IN" #INDIANA
+data2010$STATEFIP[data2010$STATEFIP == "44"]<-"RI" #RHODE ISLAND
+data2010$STATEFIP[data2010$STATEFIP == "46"]<-"SD" #SOUTH DAKOTA
+data2010$STATEFIP[data2010$STATEFIP == "20"]<-"KS" #KANSAS
+data2010$STATEFIP[data2010$STATEFIP == "41"]<-"OR" #OREGON
+data2010$STATEFIP[data2010$STATEFIP == "49"]<-"UT" #UTAH
+data2010$STATEFIP[data2010$STATEFIP == "31"]<-"NE" #NEBRASKA
+data2010$STATEFIP[data2010$STATEFIP == "23"]<-"ME" #MAINE
+data2010$STATEFIP[data2010$STATEFIP == "25"]<-"MA" #MASSACHUSSETTS
+data2010$STATEFIP[data2010$STATEFIP == "11"]<-"DC" #DISTRICT OF COLOMBIA
+data2010$STATEFIP[data2010$STATEFIP == "21"]<-"KY" #KENTUCKY
+data2010$STATEFIP[data2010$STATEFIP == "4"]<-"AZ" #ARIZONA
+data2010$STATEFIP[data2010$STATEFIP == "47"]<-"TN" #TENNESSEE
+data2010$STATEFIP[data2010$STATEFIP == "45"]<-"SC" #SOUTH CAROLINA
+data2010$STATEFIP[data2010$STATEFIP == "56"]<-"WY" #WYOMING
+data2010$STATEFIP[data2010$STATEFIP == "40"]<-"OK" #OKLAHOMA
+data2010$STATEFIP[data2010$STATEFIP == "16"]<-"ID" #IDAHO
+data2010$STATEFIP[data2010$STATEFIP == "38"]<-"ND" #NORTH DAKOTA
+data2010$STATEFIP[data2010$STATEFIP == "2"]<-"AK" #ALASKA
+data2010$STATEFIP[data2010$STATEFIP == "50"]<-"VT" #VERMONT
+data2010$STATEFIP[data2010$STATEFIP == "5"]<-"AR" #ARKANSAS
+data2010$STATEFIP[data2010$STATEFIP == "22"]<-"LA" #LOUISIANA
+data2010$STATEFIP[data2010$STATEFIP == "1"]<-"AL" #ALABAMA
+data2010$STATEFIP[data2010$STATEFIP == "54"]<-"WV" #WEST VIRGINIA
+data2010$STATEFIP[data2010$STATEFIP == "35"]<-"NM" #NEW MEXICO
+data2010$STATEFIP[data2010$STATEFIP == "28"]<-"MS" #MISSISSIPPI
+data2010$STATEFIP[data2010$STATEFIP == "30"]<-"MT" #MONTANA
 
-#---- work on adding states below starting at 33
-  
-data2010$STATEFIP[data2010$STATEFIP == "6"]<-"CA"
-data2010$STATEFIP[data2010$STATEFIP == "48"]<-"TX"
-data2010$STATEFIP[data2010$STATEFIP == "36"]<-"NY"
-data2010$STATEFIP[data2010$STATEFIP == "12"]<-"FL"
-data2010$STATEFIP[data2010$STATEFIP == "17"]<-"IL"
-data2010$STATEFIP[data2010$STATEFIP == "42"]<-"PA"
-data2010$STATEFIP[data2010$STATEFIP == "39"]<-"OH"
-data2010$STATEFIP[data2010$STATEFIP == "24"]<-"MD"
-data2010$STATEFIP[data2010$STATEFIP == "26"]<-"MI"
-data2010$STATEFIP[data2010$STATEFIP == "27"]<-"MN"
-data2010$STATEFIP[data2010$STATEFIP == "8"]<-"CO"
-data2010$STATEFIP[data2010$STATEFIP == "13"]<-"GA"
-data2010$STATEFIP[data2010$STATEFIP == "51"]<-"VA"
-data2010$STATEFIP[data2010$STATEFIP == "9"]<-"CT"
-data2010$STATEFIP[data2010$STATEFIP == "37"]<-"NC"
-data2010$STATEFIP[data2010$STATEFIP == "34"]<-"NJ"
-
+data2014$STATEFIP[data2014$STATEFIP == "6"]<-"CA" #CALIFORNIA
+data2014$STATEFIP[data2014$STATEFIP == "48"]<-"TX" #TEXAS
+data2014$STATEFIP[data2014$STATEFIP == "36"]<-"NY" #NEW YORK
+data2014$STATEFIP[data2014$STATEFIP == "12"]<-"FL" #FLORIDA
+data2014$STATEFIP[data2014$STATEFIP == "17"]<-"IL" #ILLINOIS
+data2014$STATEFIP[data2014$STATEFIP == "42"]<-"PA" #PENNSYLVANIA
+data2014$STATEFIP[data2014$STATEFIP == "39"]<-"OH" #OHIO
+data2014$STATEFIP[data2014$STATEFIP == "24"]<-"MD" #MARYLAND
+data2014$STATEFIP[data2014$STATEFIP == "26"]<-"MI" #MICHIGAN
+data2014$STATEFIP[data2014$STATEFIP == "27"]<-"MN" #MINNESOTA
+data2014$STATEFIP[data2014$STATEFIP == "8"]<-"CO" #COLORADO
+data2014$STATEFIP[data2014$STATEFIP == "13"]<-"GA" #GEORGIA
+data2014$STATEFIP[data2014$STATEFIP == "51"]<-"VA" #VIRGINIA
+data2014$STATEFIP[data2014$STATEFIP == "9"]<-"CT" #CONNECTICUT
+data2014$STATEFIP[data2014$STATEFIP == "37"]<-"NC" #NORTH CAROLINA
+data2014$STATEFIP[data2014$STATEFIP == "34"]<-"NJ" #NEW JERSEY
+data2014$STATEFIP[data2014$STATEFIP == "33"]<-"NH" #NEW HAMPSHIRE
+data2014$STATEFIP[data2014$STATEFIP == "53"]<-"WA" #WASHINGTON
+data2014$STATEFIP[data2014$STATEFIP == "19"]<-"IA" #IOWA
+data2014$STATEFIP[data2014$STATEFIP == "15"]<-"HI" #HAWAII
+data2014$STATEFIP[data2014$STATEFIP == "55"]<-"WI" #WISCONSIN
+data2014$STATEFIP[data2014$STATEFIP == "29"]<-"MO" #MISSOURI
+data2014$STATEFIP[data2014$STATEFIP == "10"]<-"DE" #DELAWARE
+data2014$STATEFIP[data2014$STATEFIP == "32"]<-"NV" #NEVADA
+data2014$STATEFIP[data2014$STATEFIP == "18"]<-"IN" #INDIANA
+data2014$STATEFIP[data2014$STATEFIP == "44"]<-"RI" #RHODE ISLAND
+data2014$STATEFIP[data2014$STATEFIP == "46"]<-"SD" #SOUTH DAKOTA
+data2014$STATEFIP[data2014$STATEFIP == "20"]<-"KS" #KANSAS
+data2014$STATEFIP[data2014$STATEFIP == "41"]<-"OR" #OREGON
+data2014$STATEFIP[data2014$STATEFIP == "49"]<-"UT" #UTAH
+data2014$STATEFIP[data2014$STATEFIP == "31"]<-"NE" #NEBRASKA
+data2014$STATEFIP[data2014$STATEFIP == "23"]<-"ME" #MAINE
+data2014$STATEFIP[data2014$STATEFIP == "25"]<-"MA" #MASSACHUSSETTS
+data2014$STATEFIP[data2014$STATEFIP == "11"]<-"DC" #DISTRICT OF COLOMBIA
+data2014$STATEFIP[data2014$STATEFIP == "21"]<-"KY" #KENTUCKY
+data2014$STATEFIP[data2014$STATEFIP == "4"]<-"AZ" #ARIZONA
+data2014$STATEFIP[data2014$STATEFIP == "47"]<-"TN" #TENNESSEE
+data2014$STATEFIP[data2014$STATEFIP == "45"]<-"SC" #SOUTH CAROLINA
+data2014$STATEFIP[data2014$STATEFIP == "56"]<-"WY" #WYOMING
+data2014$STATEFIP[data2014$STATEFIP == "40"]<-"OK" #OKLAHOMA
+data2014$STATEFIP[data2014$STATEFIP == "16"]<-"ID" #IDAHO
+data2014$STATEFIP[data2014$STATEFIP == "38"]<-"ND" #NORTH DAKOTA
+data2014$STATEFIP[data2014$STATEFIP == "2"]<-"AK" #ALASKA
+data2014$STATEFIP[data2014$STATEFIP == "50"]<-"VT" #VERMONT
+data2014$STATEFIP[data2014$STATEFIP == "5"]<-"AR" #ARKANSAS
+data2014$STATEFIP[data2014$STATEFIP == "22"]<-"LA" #LOUISIANA
+data2014$STATEFIP[data2014$STATEFIP == "1"]<-"AL" #ALABAMA
+data2014$STATEFIP[data2014$STATEFIP == "54"]<-"WV" #WEST VIRGINIA
+data2014$STATEFIP[data2014$STATEFIP == "35"]<-"NM" #NEW MEXICO
+data2014$STATEFIP[data2014$STATEFIP == "28"]<-"MS" #MISSISSIPPI
+data2014$STATEFIP[data2014$STATEFIP == "30"]<-"MT" #MONTANA
 
 #####
 # NY
@@ -192,6 +297,51 @@ female_2014NY <- data2014 %>% filter(STATEFIP == 36 & SEX == 2) %>% filter(as.nu
 View(female_2014NY)
 
 
+#####
+# TX
+#####
+
+# 2010
+# adult male educational attainment in tx - 2010
+male_2010TX <- data2010 %>% filter(STATEFIP == 48 & SEX == 1) %>% filter(as.numeric(AGE) >= 18)
+
+View(male_2010TX)
+
+# adult female educational attainment in tx - 2010
+female_2010TX <- data2010 %>% filter(STATEFIP == 48 & SEX == 2) %>% filter(as.numeric(AGE) >= 18)
+
+View(female_2010TX)
+
+# 2014
+
+# adult male educational attainment in TX - 2014
+male_2014TX <- data2014 %>% filter(STATEFIP == 48 & SEX == 1) %>% filter(as.numeric(AGE) >= 18)
+
+View(male_2014TX)
+
+# adult female educational attainment in TX - 2014
+female_2014TX <- data2014 %>% filter(STATEFIP == 48 & SEX == 2) %>% filter(as.numeric(AGE) >= 18)
+
+View(female_2014TX)
+
+
+# ALL TIME
+
+######
+###### adult female educational attainment in us across 2010-2015
+######
+female_US <- result %>% filter(SEX == 2) %>% filter(as.numeric(AGE) >= 18)
+
+View(female_US)
+
+######
+###### adult male educational attainment in us across 2010-2015
+######
+male_US <- result %>% filter(SEX == 1) %>% filter(as.numeric(AGE) >= 18)
+
+View(male_US)
+
+
 ################################################
 # STATS
 ################################################
@@ -207,6 +357,11 @@ state_count
 # in 2010
 state_count <- data2010 %>% count(STATEFIP, sort = TRUE)
 state_count
+
+# in 2012
+state_count <- data2012 %>% count(STATEFIP, sort = TRUE)
+state_count
+
 
 #################################################################
 # How many people received what level of educational attainment?
@@ -233,6 +388,33 @@ NY2010M_educ_count
 NY2010F_educ_count <- female_2010NY %>% count(EDUC, sort = TRUE)
 NY2010F_educ_count
 
+###
+
+# educational attainment in TX by males in 2014
+TX2014M_educ_count <- male_2014TX %>% count(EDUC, sort = TRUE)
+TX2014M_educ_count
+
+# educational attainment in TX by females in 2014
+TX2014F_educ_count <- female_2014TX %>% count(EDUC, sort = TRUE)
+TX2014F_educ_count
+
+# educational attainment in TX by males in 2010
+TX2010M_educ_count <- male_2010TX %>% count(EDUC, sort = TRUE)
+TX2010M_educ_count
+
+# educational attainment in TX by females in 2010
+TX2010F_educ_count <- female_2010TX %>% count(EDUC, sort = TRUE)
+TX2010F_educ_count
+
+###
+
+# educational attainment in TX by males in 2010
+US_M_educ_count <- male_US %>% count(EDUC, sort = TRUE)
+US_M_educ_count
+
+# educational attainment in TX by females in 2010
+US_F_educ_count <- female_US %>% count(EDUC, sort = TRUE)
+US_F_educ_count
 #################################################################
 # What number of people were surveyed this year? 
 # What gender did they identify with?
@@ -321,6 +503,22 @@ NYFemaleEdu2010 <- ggplot(NY2010F_educ_count, aes(EDUC, n)) +
   ggtitle("Educational Attainment by Adult Females in NY (2010)") +
   theme(axis.text.x = element_text(angle = 90))
 
+# TX Male Educational Attainment in 2010
+TXMaleEdu2010 <- ggplot(TX2010M_educ_count, aes(EDUC, n)) +
+  geom_col() +
+  xlab("Level of Educational Attainment") +
+  ylab("Count") +
+  ggtitle("Educational Attainment by Adult Males in TX (2010)") +
+  theme(axis.text.x = element_text(angle = 90))
+
+# TX Female Educational Attainment in 2010
+TXFemaleEdu2010 <- ggplot(TX2010F_educ_count, aes(EDUC, n)) +
+  geom_col() +
+  xlab("Level of Educational Attainment") +
+  ylab("Count") +
+  ggtitle("Educational Attainment by Adult Females in TX (2010)") +
+  theme(axis.text.x = element_text(angle = 90))
+
 
 # 2014
 
@@ -340,11 +538,58 @@ NYFemaleEdu2014 <- ggplot(NY2014F_educ_count, aes(EDUC, n)) +
   ggtitle("Educational Attainment by Adult Females in NY (2014)") +
   theme(axis.text.x = element_text(angle = 90))
 
+# TX Male Educational Attainment in 2014
+TXMaleEdu2014 <- ggplot(TX2014M_educ_count, aes(EDUC, n)) +
+  geom_col() +
+  xlab("Level of Educational Attainment") +
+  ylab("Count") +
+  ggtitle("Educational Attainment by Adult Males in TX (2014)") +
+  theme(axis.text.x = element_text(angle = 90))
+
+# TX Female Educational Attainment in 2014
+TXFemaleEdu2014 <- ggplot(TX2014F_educ_count, aes(EDUC, n)) +
+  geom_col() +
+  xlab("Level of Educational Attainment") +
+  ylab("Count") +
+  ggtitle("Educational Attainment by Adult Females in TX (2014)") +
+  theme(axis.text.x = element_text(angle = 90))
+
+# all time
+
+# US Male Educational Attainment
+USMaleEdu <- ggplot(US_M_educ_count, aes(EDUC, n)) +
+  geom_col() +
+  xlab("Level of Educational Attainment") +
+  ylab("Count") +
+  ggtitle("Educational Attainment by Adult Males in US (2010-2015)") +
+  theme(axis.text.x = element_text(angle = 90))
+
+# US Female Educational Attainment 
+USFemaleEdu <- ggplot(US_F_educ_count, aes(EDUC, n)) +
+  geom_col() +
+  xlab("Level of Educational Attainment") +
+  ylab("Count") +
+  ggtitle("Educational Attainment by Adult Females in US (2010-2015)") +
+  theme(axis.text.x = element_text(angle = 90))
+
 # display plots
 ggplotly(NYMaleEdu2010)
 ggplotly(NYFemaleEdu2010)
 ggplotly(NYMaleEdu2014)
 ggplotly(NYFemaleEdu2014)
+ggplotly(TXMaleEdu2010)
+ggplotly(TXFemaleEdu2010)
+ggplotly(TXMaleEdu2014)
+ggplotly(TXFemaleEdu2014)
+ggplotly(USMaleEdu)
+ggplotly(USFemaleEdu)
+
+
+### SAMPLE REGRESSION
+
+lm <- lm(EDUC ~ SEX + RACE + HISPAN, data = result)
+summary(lm)
+
 
 # closes connection to db
 dbDisconnect(conn)
